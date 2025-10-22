f26a3af9510bc8d377bbdabceb67fd134585e157
Garrett Pfingston <gpfingston@gmail.com>
2025-10-21 19:04:53 -0500

    fetched and awaited

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your code here!
+function displayPosts(users) {
+    const userList = document.querySelector('#post-list');
+
+    users.forEach(user => {
+        const listItem = document.createElement('li');
+        const h1Item = document.createElement('h1');
+        h1Item.textContent = user.title;
+        const pItem = document.createElement('p');
+        pItem.textContent = user.body;
+        listItem.appendChild(h1Item);
+        listItem.appendChild(pItem);
+        
+        userList.appendChild(listItem);
+    });
+}
+
+async function fetchAndDisplayPosts() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const users = await response.json();
+        displayPosts(users);
+    } catch (error) {
+        const userList = document.querySelector('#user-list');
+        const errorMessage = document.createElement('li');
+        errorMessage.textContent = 'Failed to load user data. Please try again later.';
+        userList.appendChild(errorMessage);
+        console.error('Error fetching user data:', error);
+    }
+}
+fetchAndDisplayPosts();
